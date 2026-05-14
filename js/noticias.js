c102604014105ab940145f6955c055276934a1d1
Emanuella Silva Novais <1046492@senacminas.edu.br>
2026-05-14 16:40:30 -0300

    implementar funcionalidade lightbox

--- js/noticias.js
+++ js/noticias.js
@@ -11,3 +11,16 @@ function alterar_tema() {
         body.classList.add("escuro");
     }
 }
+function abrir_lightbox(element){
+    var lightbox = document.getElementById("lightbox");
+    var lightbox_img = document.getElementById("lightbox-img");
+
+    lightbox_img.src = element.src;
+    lightbox.style.display = "flex";
+
+}
+
+
+function fechar_lightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
